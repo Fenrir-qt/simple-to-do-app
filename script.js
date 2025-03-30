b0a1c5a51b0f5eb4919ff87877d6040147ef2bb5
Fenrir-qt <mickburog87@gmail.com>
2025-03-30 15:55:35 +0800

    Script for saving the task in local storage

--- script.js
+++ script.js
@@ -3,7 +3,105 @@ function myFunction(){
     let taskValue = taskInput.value;
 
     if (taskValue.trim() === ""){
-        alert('Task vannot be empty!');
+        alert('Task cannot be empty!');
         return;
     }
+
+    let taskTd = document.createElement("td");
+    taskTd.textContent = taskValue;
+
+    let editTd = document.createElement("td");
+    let editBtn = document.createElement("button");
+    editBtn.textContent = "Edit";
+    editBtn.className = "btn btn-warning btn-sm"
+
+    editBtn.onclick = function(){
+        let newTask = prompt("Edit your task: ", taskValue);
+        if(newTask !== null && newTask.trim()!==""){
+            taskTd.textContent = newTask;
+            updateLocalStorage();
+        }
+    };
+
+    editTd.append(editBtn);
+
+    let deleteTd = document.createElement("td");
+    let deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "Delete";
+    deleteBtn.className = "btn btn-danger btn-sm"
+
+    deleteBtn.onclick = function(){
+        tr.remove();
+        updateLocalStorage();
+    };
+
+    deleteTd.append(deleteBtn);
+
+    let tr = document.createElement("tr");
+    tr.append(taskTd, editTd,deleteTd);
+
+    let table = document.querySelector(".todo-table");
+    table.append(tr);
+
+    updateLocalStorage();
+    taskInput.value="";
+
+};
+
+function updateLocalStorage(){
+
+    let tasks = [];
+    let rows = document.querySelectorAll(".todo-table tr:not(.list-header)");
+    rows.forEach(row =>{
+        let taskCell = row.querySelector("td");
+        if(taskCell){
+            let taskText =taskCell.textContent;
+            tasks.push(taskText);
+        }
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
 }
+
+function loadTasks(){
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+    tasks.forEach(taskValue => {
+        let taskTd = document.createElement("td");
+        taskTd.textContent = taskValue;
+    
+        let editTd = document.createElement("td");
+        let editBtn = document.createElement("button");
+        editBtn.textContent = "Edit";
+        editBtn.className = "btn btn-warning btn-sm"
+    
+        editBtn.onclick = function(){
+            let newTask = prompt("Edit your task: ", taskValue);
+    
+            if(newTask !== null && newTask.trim()!==""){
+                taskTd.textContent = newTask;
+                updateLocalStorage();
+            }
+        };
+    
+        editTd.append(editBtn);
+    
+        let deleteTd = document.createElement("td");
+        let deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
+        deleteBtn.className = "btn btn-danger btn-sm"
+    
+        deleteBtn.onclick = function(){
+            tr.remove();
+            updateLocalStorage();
+        };
+    
+        deleteTd.append(deleteBtn);
+    
+        let tr = document.createElement("tr");
+        tr.append(taskTd, editTd,deleteTd);
+    
+        let table = document.querySelector(".todo-table");
+        table.append(tr);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", loadTasks)
